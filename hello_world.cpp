a6b05d597f790a6ac8ad13cd9f9b36c482253685
Vinyack <VinyackOfficial@gmail.com>
2024-12-20 23:35:47 +0300

    Formatted code with clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 // mhm
-int main(){
-	std::string name;
-	std::cout << "Enter yout name: ";
-	std::cin >> name;
-	std::cout << "Hello World from " << name << "!" << endl;
-	return 0;
+int main() {
+  std::string name;
+  std::cout << "Enter yout name: ";
+  std::cin >> name;
+  std::cout << "Hello World from " << name << "!" << endl;
+  return 0;
 }
